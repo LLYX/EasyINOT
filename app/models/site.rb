e94747dc68e4deb4a7ed919cb0d90b425d69512e
LX <leon.xu@mail.utoronto.ca>
2015-08-17 22:57:54 -0400

    Sites replace concatenation with interpolation to avoid nil error in prepend

--- app/models/site.rb
+++ app/models/site.rb
@@ -18,8 +18,8 @@ class Site < ActiveRecord::Base
     
     private
     def prepend_sitecode
-        self.english_name = self.site_code + " " + self.english_name
-        self.french_name = self.site_code + " " + self.french_name
+        self.english_name = "#{self.site_code} #{self.english_name}"
+        self.french_name = "#{self.site_code} #{self.french_name}"
     end
 
     def self.search(search)
